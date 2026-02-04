c277d4fe70d0a9c17199a91c681657107a9df4e4
Mateusz <matkarczewski92@gmail.com>
2026-02-04 18:37:31 +0100

    create

---
+++ app/Livewire/Admin/SystemConfigPanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class SystemConfigPanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.system-config-panel');
+    }
+}
---
+++ app/Livewire/Animals/Profile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Animals;
+
+use Livewire\Component;
+
+class Profile extends Component
+{
+    public function render()
+    {
+        return view('livewire.animals.profile');
+    }
+}
